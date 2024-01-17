10629d0c6165428b845aba7ae373c74f6d3e777a
ajit51 <ajitk433@gmail.com>
2024-01-18 04:27:59 +0530

    first commit

---
+++ src/main/java/com/javaguides/mokitopracticeexample/MokitoPracticeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.javaguides.mokitopracticeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokitoPracticeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokitoPracticeExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaguides/mokitopracticeexample/MokitoPracticeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaguides.mokitopracticeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokitoPracticeExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
