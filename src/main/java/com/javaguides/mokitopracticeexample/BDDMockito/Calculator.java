7c54da0089f5e0d7827f2c70023b10e08ab66316
ajit51 <ajitk433@gmail.com>
2024-01-27 20:19:14 +0530

    example of BDDMockito willCallRealMethod() method

--- src/main/java/com/javaguides/mokitopracticeexample/BDDMockito/Calculator.java
+++ src/main/java/com/javaguides/mokitopracticeexample/BDDMockito/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     double add(double a, double b) {
         return a + b;
     }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
 }
--- src/test/java/com/javaguides/mokitopracticeexample/BDDMockito/CalculatorBDDTest.java
+++ src/test/java/com/javaguides/mokitopracticeexample/BDDMockito/CalculatorBDDTest.java
@@ -1,6 +1,8 @@
 package com.javaguides.mokitopracticeexample.BDDMockito;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 
 public class CalculatorBDDTest {
@@ -15,5 +17,18 @@ public class CalculatorBDDTest {
         BDDMockito.then(mockCalculator).should(BDDMockito.times(1)).add(10.0, 20.0);
         System.out.println(result);
 
+    }
+
+    @Test
+    public void testAddMethod() {
+        // Given: Creating the mock and setting up the scenario
+        Calculator mockCalculator = BDDMockito.mock(Calculator.class);
+        // Mocking subtract while letting add execute its real implementation
+        BDDMockito.given(mockCalculator.subtract(ArgumentMatchers.anyDouble(), ArgumentMatchers.anyDouble())).willReturn(0.0);
+        BDDMockito.willCallRealMethod().given(mockCalculator).add(ArgumentMatchers.anyDouble(), ArgumentMatchers.anyDouble());
+        // When: Performing the actions
+        double result = mockCalculator.add(3.0, 4.0);
+        // Then: Verifying the results
+        Assertions.assertEquals(7.0, result);
     }
 }
